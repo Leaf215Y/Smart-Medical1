bd25fe940e5a018113003d748dc73f788db986ed
juyoumimao <1831297562@qq.com>
2025-06-23 11:13:35 +0800

    数据字典crud
    数据字典crud

---
+++ src/Smart_Medical.Application.Contracts/Dictionarys/DictionaryDatas/GetDictionaryDataSearchDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Medical.Dictionarys.DictionaryDatas
+{
+    public class GetDictionaryDataSearchDto
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+
+    }
+}
---
+++ src/Smart_Medical.Application.Contracts/Dictionarys/DictionaryTypes/IDictionaryTypeService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smart_Medical.Until;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Smart_Medical.Dictionarys.DictionaryTypes
+{
+    public interface IDictionaryTypeService:IApplicationService
+    {
+        Task<ApiResult<PageResult<List<GetDictionaryTypeDto>>>> GetDictionaryDataList([FromQuery] GetDictionaryTypeSearchDto search);
+    }
+}
